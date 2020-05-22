8af270fe6af68546a25d9d02205a5216bd8e96e2
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-21 21:35:31 -0600

    Sorting blog posts from newest to oldest. Added styled components, and began simple styling.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Jayden's Blog!`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -22,6 +22,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -14,7 +23,11 @@ const IndexPage = ({ data }) => (
       {
         data.allMarkdownRemark.edges.map(({node}) => (
           <div key={node.id}>
-            <span>{ node.frontmatter.title } - { node.frontmatter.date }</span>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                { node.frontmatter.title } - { node.frontmatter.date }
+              </BlogTitle>
+            </BlogLink>
             <p>{ node.excerpt }</p>
           </div>
         ))
@@ -26,7 +39,7 @@ const IndexPage = ({ data }) => (
 export default IndexPage
 export const query = graphql`
   query MyQuery {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC}) {
       totalCount
       edges {
         node {
@@ -35,6 +48,9 @@ export const query = graphql`
             description
             title
             date
+          }
+          fields {
+            slug
           }
           excerpt
         }
